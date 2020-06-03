8c359f1fd7e008791e2d960a9413001b6d17d562
Yi Lin Lee <yilinlee19@gmail.com>
2020-06-04 00:25:13 +1000

    added form and answer comparison

--- script.js
+++ script.js
@@ -3,34 +3,73 @@ var questionEl = $("#questionDiv");
 var answerEl = $("#answerDiv");
 var timerEl = $("#timeLeft");
 
-// Questions
-var qArray = [1, 2, 3, 4];
-var q1 = "Question 1";
-var q1a1 = "Question 1 Answer 1";
-var q1a2 = "Question 1 Answer 2";
-var q1a3 = "Question 1 Answer 3";
-var q1a4 = "Question 1 Answer 4";
+var timeLeft = 60;
+var score = 0;
+var qNumber = 1;  // Starting with Question 1
+var endGame;
+
+// Creating questions and answers
+var allQuestions = {
+    1: {
+        question: "question 1",
+        answers: ["answer 1", "answer 2", "answer 3", "answer 4"],
+        correct: 1,
+        },
+    2: {
+        question: "question 2",
+        answers: ["answer 1", "answer 2", "answer 3", "answer 4"],
+        correct: 2,
+        },
+    3: {
+        question: "question 3",
+        answers: ["answer 1", "answer 2", "answer 3", "answer 4"],
+        correct: 3,
+        },
+    4: {
+        question: "question 4",
+        answers: ["answer 1", "answer 2", "answer 3", "answer 4"],
+        correct: 4,
+        },    
+}
 
 // Quiz welcome page with "Start Quiz" button
 answerEl.append(`<button id="startButton">Start Quiz</button>`);
 
-var timeLeft = 60;
-var qNumber = 0;
-
+// Handle loading quiz
 var loadQuiz = function() {
+    // Handle ending game
+    endGame = function() {
+        // Clear timer
+        clearInterval(timeInterval);
+        
+        // Create form for submitting score
+        $(questionEl).html("All done!");
+        $(answerEl).html("Your final score is " + score);
+        var nameForm = $(`<form><input class="form-control" type="text" placeholder="Enter your name into the leaderboard"></form`);
+        $(answerEl).append(nameForm);
+
+        // On submit score, store name on local storage, order highscores from highest to lowest, display highscores
+        $(nameForm).submit(function(e) {
+            e.preventDefault();
+            console.log("submitteddd");
+        });
+
+        // Add button to clear highscores
+        var clearHighscores = $(`<button type="button" class="btn btn-secondary">`+`Clear Highscores`+`</button>`);
+    }
+    
     // Timer
     var timeInterval = setInterval(function() {
-        // Decrease time and display it
-        timeLeft--;
-        $(timerEl).html(timeLeft);
-
-        if (timeLeft === 0) {
-            clearInterval(timeInterval);
-            // End game()
-            console.log("engGame");
-        }
-
-    }, 1000);
+            // Decrease time and display it
+            timeLeft--;
+            $(timerEl).html(timeLeft);
+    
+            // If timer hits 0, end game
+            if (timeLeft === 0) {
+                clearInterval(timeInterval);
+                endGame();
+            }
+        }, 1000);
 
     // Load first question
     loadQuestion();
@@ -40,59 +79,46 @@ var loadQuiz = function() {
 var loadQuestion = function() {
     console.log("loadQuestion")
     // If there is a next question, load question
-    if(qArray[qNumber]){
+    if(allQuestions[qNumber]){
         console.log("next question")
-        $(questionEl).html(qArray[qNumber]);
-        var allAnswers = $("<div>");
-        var possAnswers = ["answer1", "answer2", "answer3", "answer4"];
+        console.log(allQuestions[qNumber])
+        // Display question from allQuestions object
+        $(questionEl).html(allQuestions[qNumber].question);
+        // Display answers from allQuestions object
+        var answerOptions = $("<div>");
+        var possAnswers = allQuestions[qNumber].answers;
         possAnswers.forEach(function(answer) {
-            $(allAnswers).append("<button class='aButton'>" + answer + "</button>");
-            console.log(allAnswers);
+            $(answerOptions).append(`<button type="button" class="aButton btn btn-outline-secondary">` + answer + `</button>`);
+            console.log(answerOptions);
         })
-        $(answerEl).html(allAnswers);
+        $(answerEl).html(answerOptions);
     } else {
-        // endGame()
-        console.log("endGame");
+        // End game
+        endGame();
     }
-    qNumber++;
-
-    // On clicking an answer button, check answer
-    // Handle points / timer updates
 
     // On clicking an answer button, load next question
-    $(".aButton").on("click", loadQuestion);
+    $(".aButton").on("click", handleAnswerClick);
+}
+
+// Handle when player clicks an answer
+var handleAnswerClick = function(e) {
+    // Check answer
+    console.log("checking answer");
+    var playerAnswer = allQuestions[qNumber].answers.indexOf(e.target.innerHTML) + 1;
+    if(playerAnswer === allQuestions[qNumber].correct){
+        // Add points
+        score = score + 10;
+        console.log("correct! add points");
+    } else {
+        // Subtract time from timer
+        timeLeft = timeLeft - 10;
+        console.log("wrong! taking time away from you")
+    }
+    // Load next question
+    qNumber++;  // Increase question number
+    loadQuestion();
 }
 
 // On clicking "Start Quiz", timer starts
 $("#startButton").on("click", loadQuiz);
-
-
-
-// Create variable for score
-
-// Create questions and answers
-// q1 = "laskdjfsdlkfj?"
-// q1a1 = {answer: "slkdfjslkfdj", correct: true}
-// q1a2 = {answer: "sdfsdfassdfs", correct: false}
-// q1a3 = {answer: "sdfdgdfgsdfs", correct: false}
-// q1a4 = {answer: "dfgsdasfsdfs", correct: false}
-// q2 = "laskdjfsdlkfj?"
-// q2a1 = {answer: "slkdfjslkfdj", correct: true}
-// q2a2 = {answer: "sdfsdfassdfs", correct: false}
-// q2a3 = {answer: "sdfdgdfgsdfs", correct: false}
-// q2a4 = {answer: "dfgsdasfsdfs", correct: false}
-
-// for each question:
-//// Display the question
-//// Display the possible answers
-//// On click event on an answer, compare user's answer with correct answer
-////// If correct, add to score
-////// If wrong, subtract time from timer
-
-// When timer hits 0, game is over (while loop??)
-
-// When all questions are completed, game is over
-// Display form to submit initials
-
-// Order scores from highest to lowest
-// Display highscores on page
