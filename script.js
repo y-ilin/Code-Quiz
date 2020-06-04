b59db846ac7e2e21bb85b1693d82529e063dbe85
Yi Lin Lee <yilinlee19@gmail.com>
2020-06-05 01:27:39 +1000

    added local storage for highscores

--- script.js
+++ script.js
@@ -1,65 +1,61 @@
+// ADDDD DOCUMENT.READYYYYYYYYYYYYYYYY /////////////////////////////////////////////////////
+///////////////////////////////////////
+////////////////////////////////////////////////
+
 // Grab HTML elements
-var questionEl = $("#questionDiv");
-var answerEl = $("#answerDiv");
+var mainTitleEl = $("#mainTitleEl");
+var mainBodyEl = $("#mainBodyEl");
 var timerEl = $("#timeLeft");
 
 var timeLeft = 60;
 var score = 0;
 var qNumber = 1;  // Starting with Question 1
-var endGame;
+var timeInterval;
+
+// Getting existing highscores from local storage
+var highscoresObj = {};
+if( localStorage.getItem("highscoresString") ){
+    highscoresObj = JSON.parse(localStorage.getItem("highscoresString"));
+}
+
+// Getting previous playerId from local storage
+var playerId = 1;
+if( localStorage.getItem("playerId")) {
+    playerId = parseInt(localStorage.getItem("playerId")) + 1;
+}
+// Setting current playerId
+localStorage.setItem("playerId", playerId);
+
 
 // Creating questions and answers
 var allQuestions = {
-    1: {
-        question: "question 1",
+    1: {question: "question 1",
         answers: ["answer 1", "answer 2", "answer 3", "answer 4"],
         correct: 1,
         },
-    2: {
-        question: "question 2",
+    2: {question: "question 2",
         answers: ["answer 1", "answer 2", "answer 3", "answer 4"],
         correct: 2,
         },
-    3: {
-        question: "question 3",
+    3: {question: "question 3",
         answers: ["answer 1", "answer 2", "answer 3", "answer 4"],
         correct: 3,
         },
-    4: {
-        question: "question 4",
+    4: {question: "question 4",
         answers: ["answer 1", "answer 2", "answer 3", "answer 4"],
         correct: 4,
         },    
 }
 
 // Quiz welcome page with "Start Quiz" button
-answerEl.append(`<button id="startButton">Start Quiz</button>`);
+mainBodyEl.append(`<button id="startButton">Start Quiz</button>`);
+
 
 // Handle loading quiz
 var loadQuiz = function() {
-    // Handle ending game
-    endGame = function() {
-        // Clear timer
-        clearInterval(timeInterval);
-        
-        // Create form for submitting score
-        $(questionEl).html("All done!");
-        $(answerEl).html("Your final score is " + score);
-        var nameForm = $(`<form><input class="form-control" type="text" placeholder="Enter your name into the leaderboard"></form`);
-        $(answerEl).append(nameForm);
-
-        // On submit score, store name on local storage, order highscores from highest to lowest, display highscores
-        $(nameForm).submit(function(e) {
-            e.preventDefault();
-            console.log("submitteddd");
-        });
-
-        // Add button to clear highscores
-        var clearHighscores = $(`<button type="button" class="btn btn-secondary">`+`Clear Highscores`+`</button>`);
-    }
-    
+    console.log(playerId);
     // Timer
-    var timeInterval = setInterval(function() {
+    timeInterval = setInterval(function() {
             // Decrease time and display it
             timeLeft--;
             $(timerEl).html(timeLeft);
@@ -83,7 +79,7 @@ var loadQuestion = function() {
         console.log("next question")
         console.log(allQuestions[qNumber])
         // Display question from allQuestions object
-        $(questionEl).html(allQuestions[qNumber].question);
+        $(mainTitleEl).html(allQuestions[qNumber].question);
         // Display answers from allQuestions object
         var answerOptions = $("<div>");
         var possAnswers = allQuestions[qNumber].answers;
@@ -91,7 +87,7 @@ var loadQuestion = function() {
             $(answerOptions).append(`<button type="button" class="aButton btn btn-outline-secondary">` + answer + `</button>`);
             console.log(answerOptions);
         })
-        $(answerEl).html(answerOptions);
+        $(mainBodyEl).html(answerOptions);
     } else {
         // End game
         endGame();
@@ -120,5 +116,64 @@ var handleAnswerClick = function(e) {
     loadQuestion();
 }
 
+// Handle ending game
+var endGame = function() {
+    // Display time left
+    $(timerEl).html(timeLeft);
+
+    // Clear timer
+    clearInterval(timeInterval);
+    
+    // Create form for submitting score
+    $(mainTitleEl).html("All done!");
+    $(mainBodyEl).html("Your final score is " + score);
+    var nameForm = $(`<form><input class="form-control" id="nameForm" type="text" placeholder="Enter your name into the leaderboard"></form`);
+    $(mainBodyEl).append(nameForm);
+
+    // On submit score, store name on local storage, order highscores from highest to lowest, display highscores
+    $(nameForm).submit(function(e) {
+        e.preventDefault();
+        console.log("submitteddd");
+
+        // Store name to local storage
+        var newName = $("#nameForm")[0].value;
+        
+        if (newName === "") {
+            $("form").append(`<div id="nameEnterError">Name cannot be blank</div>`);
+        } else {
+            highscoresObj[playerId] = {id: playerId, name: newName, score: score};
+            localStorage.setItem("highscoresString", JSON.stringify(highscoresObj));
+            console.log(playerId)
+            $("form").append(`<div id="nameEnterError">Ye boi your name has been entered!</div>`);
+
+            // Sort highscores from highest to lowest
+            console.log(JSON.parse(localStorage.getItem("highscoresString")));
+            var highscoresSorted = Object.values(highscoresObj).sort((a, b) => (a.score > b.score) ? -1 : 1);
+
+            // Load highscores page
+            highscoresSorted.forEach( function(player) {
+                $(mainBodyEl).append("<p>"+player.name+player.score+"</p>");
+            })
+        }
+
+    });
+};
+
 // On clicking "Start Quiz", timer starts
 $("#startButton").on("click", loadQuiz);
+
+var loadHighscores = function() {
+    // Display "Highscores" title
+    $(mainTitleEl).html("Highscores");
+    // Order highscores from highest to lowest
+
+    // Display highscores
+
+    // Add button to clear highscores
+    var clearHighscores = function() {
+        highscoresObj = {};
+        // playerId = 1;
+    }
+    var clearHighscoresButton = $(`<button type="button" class="btn btn-secondary">`+`Clear Highscores`+`</button>`);
+    $(clearHighscoresButton).on("click", clearHighscores)
+}
